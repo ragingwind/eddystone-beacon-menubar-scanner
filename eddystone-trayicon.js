4bc80942eddc0862928cc423b3db26c41fd52843
ragingwind <ragingwind@gmail.com>
2015-09-11 22:37:11 +0900

    Add click event and icon image

---
+++ eddystone-trayicon.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Menu = require('menu');
+const Tray = require('tray');
+const shell = require('shell');
+
+function EddystoneTrayIcon(opts) {
+	opts = opts || {};
+
+	this.trayIcon = new Tray(opts.iconForNotFound);
+
+	if (opts.toolTip) {
+		this.trayIcon.setToolTip(opts.toolTip);
+	}
+
+	if (opts.iconForPressed) {
+		this.trayIcon.setPressedImage(opts.iconForPressed);
+	}
+	
+	this.opts = opts;
+	this.beacons = [];
+	this.refresh();
+}
+
+EddystoneTrayIcon.prototype.refresh = function () {
+	var contextMenu = null;
+
+	if (this.beacons.length > 0) {
+		this.trayIcon.setImage(this.opts.iconForFound);
+		
+		contextMenu = Menu.buildFromTemplate(this.beacons.map(function (beacon) {
+			return {
+				label: [
+					beacon.url, ',', 'RX Power:',
+					beacon.txPower, ',', 'RSSI',
+					beacon.rssi
+				].join(' '),
+				click: function () {
+					shell.openExternal(beacon.url);
+				}
+			};
+		}));
+	} else {
+		this.trayIcon.setImage(this.opts.iconForNotFound);
+
+		contextMenu = Menu.buildFromTemplate([{label: 'Not found'}]);
+	}
+	
+	this.trayIcon.setContextMenu(contextMenu);
+};
+
+EddystoneTrayIcon.prototype.add = function (beacon) {
+	this.beacons.push(beacon);
+	this.refresh();
+};
+
+EddystoneTrayIcon.prototype.clear = function (beacon) {
+	this.beacons = [];
+	this.refresh();
+};
+
+module.exports = EddystoneTrayIcon;
--- index.js
+++ index.js
@@ -1,68 +1,16 @@
 'use strict';
 
 const app = require('app');
-const Menu = require('menu');
-const Tray = require('tray');
 const scanner = require('eddystone-beacon-scanner');
+const EddystoneTrayIcon = require('./eddystone-trayicon');
+const SCANNING_INTERVAL = 1000 * 30;
 
 // report crashes to the Electron project
 require('crash-reporter').start();
 
-// enable debug window
-require('electron-debug')();
-
-function EddystoneTrayIcon(opts) {
-	opts = opts || {};
-
-	if (!opts.icon) {
-		throw new Error('Eddystone tray icon must be exist');
-	}
-
-	this.trayIcon = new Tray(opts.icon);
-
-	if (opts.toolTip) {
-		this.trayIcon.setToolTip(opts.toolTip);
-	}
-
-	if (opts.iconForPressed) {
-		this.trayIcon.setPressedImage(opts.iconForPressed);
-	}
-
-	this.beacons = [];
-	this.refresh();
-}
-
-EddystoneTrayIcon.prototype.refresh = function () {
-	var contextMenu = null;
-
-	if (this.beacons.length > 0) {
-		contextMenu = Menu.buildFromTemplate(this.beacons.map(function(beacon) {
-			return {
-				label: [
-					beacon.url, ',', 'RX Power:',
-					beacon.txPower, ',', 'RSSI',
-					beacon.rssi
-				].join(' ')
-			};
-		}));
-	} else {
-		contextMenu = Menu.buildFromTemplate([{label: 'Not found'}]);
-	}
-	
-	this.trayIcon.setContextMenu(contextMenu);
-};
-
-EddystoneTrayIcon.prototype.add = function (beacon) {
-	this.beacons.push(beacon);
-	this.refresh();
-};
-
-EddystoneTrayIcon.prototype.clear = function (beacon) {
-	this.beacon = [];
-	this.refresh();
-};
-
 app.on('window-all-closed', function () {
+	clearTimeout(app.scanTimer);
+	
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
@@ -71,14 +19,21 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
 	app.trayIcon = new EddystoneTrayIcon({
 		toolTip: 'Eddystone beacon scanner',
-		icon: 'images/lighthouse-grey-16x16.png',
+		iconForNotFound: 'images/lighthouse-grey-16x16.png',
+		iconForFound: 'images/lighthouse-black-16x16.png',
 		iconForPressed: 'images/lighthouse-white-16x16.png'
 	});
 	
 	scanner.on('discover', function(beacon) {
-		console.log('beacon found:\b\t', beacon);
+		console.log('beacon has been found:', beacon.url);
 		app.trayIcon.add(beacon);
 	});
 	
 	scanner.startScanning();
+	
+	app.scanTimer = setInterval(function () {
+		console.log('Start beacon scanning');
+		app.trayIcon.clear();
+		scanner.startScanning();
+	}, SCANNING_INTERVAL);
 });
